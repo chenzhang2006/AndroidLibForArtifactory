5c3f7911dc092461648e462a4d0ed7b16d1234ee
Chen <chen.zhang3@wework.com>
2019-11-21 13:55:53 -0500

    test success with consuming app

---
+++ app/src/main/java/com/chenzhang/androidlibforartifactory/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.chenzhang.androidlibforartifactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chenzhang.droidlibrary.doubleLength
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        message.text = "welcome".doubleLength().toString()
+    }
+}
