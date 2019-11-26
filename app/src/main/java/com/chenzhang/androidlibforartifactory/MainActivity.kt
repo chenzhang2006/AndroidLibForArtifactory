0f49889e00ebe95196fed7d987b63773a52db248
Chen Zhang <chen.zhang@jet.com>
2019-11-26 18:59:48 -0500

    fix activity compile errors

--- app/src/main/java/com/chenzhang/androidlibforartifactory/MainActivity.kt
+++ app/src/main/java/com/chenzhang/androidlibforartifactory/MainActivity.kt
@@ -2,7 +2,7 @@ package com.chenzhang.androidlibforartifactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.chenzhang.droidlibrary.doubleLength
+//import com.chenzhang.droidlibrary.doubleLength
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "welcome".doubleLength().toString()
+//        message.text = "welcome".doubleLength().toString()
     }
 }
