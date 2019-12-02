51ea9dc4032dcbb4856ea39909045f487a049ff7
Chen <chen.zhang3@wework.com>
2019-12-02 11:43:45 -0500

    update library 1.1.9

--- droidlibrary/src/main/java/com/chenzhang/droidlibrary/StringExt.kt
+++ droidlibrary/src/main/java/com/chenzhang/droidlibrary/StringExt.kt
@@ -1,5 +1,3 @@
 package com.chenzhang.droidlibrary
 
-fun String.getLengthMessage(): String = "\"$this\" has ${this.length} characters"
-
 fun String.getPublishingMessage(): String = "\"$this\" from Github Actions"
