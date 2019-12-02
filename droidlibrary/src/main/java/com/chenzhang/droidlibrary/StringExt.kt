fd13a662524b20a38a0d2b99598ce5529a4f11bd
Chen <chen.zhang3@wework.com>
2019-12-02 11:40:57 -0500

    publish with CI

--- droidlibrary/src/main/java/com/chenzhang/droidlibrary/StringExt.kt
+++ droidlibrary/src/main/java/com/chenzhang/droidlibrary/StringExt.kt
@@ -2,4 +2,4 @@ package com.chenzhang.droidlibrary
 
 fun String.getLengthMessage(): String = "\"$this\" has ${this.length} characters"
 
-fun String.getPublishingMessage(): String = "\"$this\" from manual publishing"
+fun String.getPublishingMessage(): String = "\"$this\" from Github Actions"
