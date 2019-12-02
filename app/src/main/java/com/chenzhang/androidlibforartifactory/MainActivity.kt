f0faf05f5719e3571e3b1fb08a5bbdb4ab2b65e5
Chen <chen.zhang3@wework.com>
2019-12-02 11:35:33 -0500

    success manual publish/consume

--- app/src/main/java/com/chenzhang/androidlibforartifactory/MainActivity.kt
+++ app/src/main/java/com/chenzhang/androidlibforartifactory/MainActivity.kt
@@ -2,7 +2,7 @@ package com.chenzhang.androidlibforartifactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.chenzhang.droidlibrary.getLengthMessage
+import com.chenzhang.droidlibrary.getPublishingMessage
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "welcome".getLengthMessage()
+        message.text = "welcome".getPublishingMessage()
     }
 }
--- droidlibrary/src/main/java/com/chenzhang/droidlibrary/StringExt.kt
+++ droidlibrary/src/main/java/com/chenzhang/droidlibrary/StringExt.kt
@@ -1,3 +1,5 @@
 package com.chenzhang.droidlibrary
 
 fun String.getLengthMessage(): String = "\"$this\" has ${this.length} characters"
+
+fun String.getPublishingMessage(): String = "\"$this\" from manual publishing"
