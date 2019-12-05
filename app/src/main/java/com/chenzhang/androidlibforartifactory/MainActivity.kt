bf1f4d2dd63ee359cb2f23fe3e5a372410b7b36a
Chen <chen.zhang3@wework.com>
2019-12-05 16:58:21 -0500

    demo transitive dependency

--- app/src/main/java/com/chenzhang/androidlibforartifactory/MainActivity.kt
+++ app/src/main/java/com/chenzhang/androidlibforartifactory/MainActivity.kt
@@ -3,6 +3,7 @@ package com.chenzhang.androidlibforartifactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.chenzhang.droidlibrary.getPublishingMessage
+import com.chenzhang.droidlibrary2.getDecoratedMessage
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        message.text = "welcome".getPublishingMessage()
+        message.text = "welcome".getDecoratedMessage() + "welcome".getPublishingMessage()
     }
 }
---
+++ droidlibrary2/src/main/java/com/chenzhang/droidlibrary2/StringExt2.kt
@@ -0,0 +1,5 @@
+package com.chenzhang.droidlibrary2
+
+import com.chenzhang.droidlibrary.getPublishingMessage
+
+fun String.getDecoratedMessage() = this.getPublishingMessage() + ", Yey!"
